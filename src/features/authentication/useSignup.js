1738b325b2fa20c0841fbce23b568f1d0156e03a
Farhan Mirza <farhanmirza0614@gmail.com>
2026-09-01 21:32:49 +0500

    navigating the user to dashboard when he signup

--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -1,14 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { signup as signupAPI } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useSignup() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
   const { mutate: signup, isPending: isLoading } = useMutation({
     mutationFn: signupAPI,
     onSuccess: (data) => {
-      toast.success(
-        "Account is successfully created! Please verify your account from your gmail inbox",
-      );
+      queryClient.setQueryData(["user"], data.user);
+      toast.success("Account is successfully created!");
+      navigate("/dashboard");
     },
   });
 
