42e24202ef85ce25dca994fc8227936ed6966640
Farhan Mirza <farhanmirza0614@gmail.com>
2026-09-14 20:22:21 +0500

    ai model change

--- api/index.py
+++ api/index.py
@@ -305,8 +305,8 @@ def generate_future_report(user_id: str):
         response = client.models.generate_content(
             # model='gemma-3-27b-it',
             # model='gemini-2.5-flash',
-             model='gemma-4-31b-it',
-            # model='gemma-4-26b-a4b-it',
+            #  model='gemma-4-31b-it',
+            model='gemma-4-26b-a4b-it',
             contents=prompt
         )
         ai_text = response.text
